3904a65638fb57ef809b87e6ab3520ee5603b4c6
Wung Jae Lee <wunglee2019@u.northwestern.edu>
2017-07-11 15:23:21 +0900

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -7,12 +7,54 @@ console.log('Starting redux example');
 // 2. no global variables
 // 3. synchronous
 
-var reducer = (state = {name: 'anonymous'}, action) => {
+var stateDefault = {
+  name: 'anonymous',
+  hobbies: [],
+  movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            hobby: action.hobby,
+            id: nextHobbyId++
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            movieTitle: action.movieTitle,
+            movieGenre: action.movieGenre,
+            id: nextMovieId++
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
       };
     default:
       return state;
@@ -28,6 +70,7 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+  console.log(state);
 });
 // unsubscribe();
 
@@ -43,6 +86,38 @@ store.dispatch({
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'swimming'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movieTitle: 'Avengers',
+  movieGenre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movieTitle: 'Lucky for you',
+  movieGenre: 'Thats what I like'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
 });
 
 
